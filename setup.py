3bba1a75063f554f8e5c062d3b4ba892fdb07218
Mark Hegedus <hmark33@gmail.com>
2019-08-07 10:37:14 +0200

    fix(setup.py): add dependencies list

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='pyheart',
-    version='0.0.2',
+    version='0.0.5',
     description='A simple Python3 heartbeat package.',
     py_modules=["pyheart"],
+    install_requires=['logging>=0.4.9.6','requests-toolbelt>=0.9.1'],
     package_dir={'': 'src'},
 )
